3d65d6969b0c8438addfb897b11af27fb7813cc0
djurik2008 <139246468+djurik2008@users.noreply.github.com>
2024-09-26 12:32:44 +0000

    Deploying to gh-pages from @ djurik2008/my-camper@c4f852d964f7adb214a81cbf89c895b1a0558292 🚀

---
+++ assets/CatalogPage-sd4TjK7c.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CE5a4HMW.js";const t=()=>e.jsx("h1",{children:"Favorites Page"});export{t as default};
---
+++ assets/HomePage-fPdobT-x.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CE5a4HMW.js";const r=()=>e.jsx("h1",{children:"Home Page"});export{r as default};
