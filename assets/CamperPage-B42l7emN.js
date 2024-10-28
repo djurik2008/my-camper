4dffcf81fa025349fd3a031282c712ca250dfb45
djurik2008 <139246468+djurik2008@users.noreply.github.com>
2024-10-28 15:16:33 +0000

    Deploying to gh-pages from @ djurik2008/my-camper@9179559805c0cbdef42fd90d4524012bba8fffb8 🚀

--- assets/CamperPage-B42l7emN.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-63p3AgXt.js";const t=()=>e.jsx("h1",{children:"Camper details"});export{t as default};
---
+++ assets/LinkButton-_fC70SSS.js
@@ -0,0 +1,1 @@
+import{j as o,L as s}from"./index-BrNUR70v.js";const i="_linkButton_ele6f_88",e={linkButton:i},r=({text:t,linkTo:n})=>o.jsx(s,{to:n,className:e.linkButton,children:t});export{r as L};
