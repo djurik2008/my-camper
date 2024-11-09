cfb70b2782b8fc9086877f3546a36ddda9db7866
djurik2008 <139246468+djurik2008@users.noreply.github.com>
2024-11-09 07:27:56 +0000

    Deploying to gh-pages from @ djurik2008/my-camper@78f8281c82f6df7a14b0f81d8141ad0be03a0342 🚀

---
+++ assets/LinkButton-BriIyAjZ.js
@@ -0,0 +1,1 @@
+import{j as o,L as s}from"./index-FDGW3VIf.js";const i="_linkButton_wfbfd_25",r={linkButton:i},k=({text:t,linkTo:n})=>o.jsx(s,{to:n,className:r.linkButton,children:t});export{k as L};
